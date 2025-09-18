878ed6f162e6a4afc947795bd653b502f7fbd601
krishnaT07 <160775556+krishnaT07@users.noreply.github.com>
2025-09-19 02:01:20 +0530

    added debugging in db file

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
 
-
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT
-    ).then(() => {
-        console.log('Connected to DB');
-    }).catch(err => console.log(err));
-}
+  const mongoUri = process.env.MONGO_URI; // Make sure this matches your Render env variable
+  if (!mongoUri) {
+    console.error('Error: MONGO_URI is not defined in environment variables!');
+    process.exit(1);
+  }
 
+  mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
+}
 
 module.exports = connectToDb;
